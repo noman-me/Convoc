7f5e0c32f76bb8b1d7695d3c83e9b5e0b93597ad
Noman Abdullah <77800236+noman-me@users.noreply.github.com>
2021-10-16 05:29:52 +0600

    Add files via upload

---
+++ Conv/assets/js/main.js
@@ -0,0 +1,133 @@
+/*-----------------------------------------------------------------------------------
+    Template Name: #
+    Template URI: #
+    Author:
+    Author URI:  #
+    Version: 1.0
+
+    Note: This is Main Js file
+-----------------------------------------------------------------------------------
+    Js INDEX
+    ===================
+    ## Main Menu
+    ## Sticky Header
+    ## Header Breakpoint Resize
+    ## Counter Up
+    ## Project Progress Bar
+    ## Project Slider One
+    ## Project Slider Two
+    ## Team Slider
+    ## Active Tooltip
+    ## Image Block slider
+    ## Preloader
+-----------------------------------------------------------------------------------*/
+
+(function ($) {
+    'use strict';
+
+    // ===== Main Menu
+    function mainMenu() {
+        const navbarToggler = $('.nav-toggler'),
+            navMenu = $('.nav-menu'),
+            mobilePanel = $('.mobile-menu-panel'),
+            mobilePanelMenu = $('.mobile-menu-panel .panel-menu'),
+            navClose = $('.panel-close');
+
+        // Navbar toggler
+        navbarToggler.on('click', function (e) {
+            e.preventDefault();
+
+            navbarToggler.toggleClass('panel-opened');
+            mobilePanel.toggleClass('panel-opened');
+        });
+
+        // Close icon
+        navClose.on('click', function (e) {
+            e.preventDefault();
+
+            mobilePanel.removeClass('panel-opened');
+            navbarToggler.removeClass('panel-opened');
+        });
+
+        // Adds toggle button to li items that have children
+        navMenu.find('li a').each(function () {
+            if ($(this).next().length > 0) {
+                $(this).append('<span class="dd-trigger"><i class="far fa-angle-down"></i></span>');
+            }
+        });
+
+        mobilePanelMenu.find('li a').each(function () {
+            if ($(this).next().length > 0) {
+                $(this).append('<span class="dd-trigger"><i class="far fa-angle-down"></i></span>');
+            }
+        });
+
+        // Expands the dropdown menu on each click
+        mobilePanelMenu.find('.dd-trigger').on('click', function (e) {
+            e.preventDefault();
+            $(this).parent().parent('li').children('ul.submenu').stop(true, true).slideToggle(350);
+            $(this).toggleClass('submenu-opened')
+        });
+    };
+
+    // ===== Sticky Header
+    function stickyHeader() {
+        let scroll_top = $(window).scrollTop(),
+            siteHeader = $('.site-header'),
+            offsetTop = siteHeader.outerHeight(),
+            offsetTopAnimation = offsetTop + 150;
+
+        if (siteHeader.hasClass('sticky-header')) {
+            if (scroll_top > offsetTopAnimation) {
+                siteHeader.addClass('sticky-on');
+            } else {
+                siteHeader.removeClass('sticky-on');
+            }
+        }
+    }
+
+    // ===== Header Breakpoint Resize
+    function headerBreakpointResize() {
+        // Breakpoint Check
+        const windowWidth = $(window).innerWidth(),
+            navContainer = $('.navbar-wrapper'),
+            breakpoint = 992;
+
+        if (windowWidth <= breakpoint) {
+            navContainer.addClass('breakpoint-on');
+        } else {
+            navContainer.removeClass('breakpoint-on');
+        }
+
+        // Header Height
+        const siteHeader = $('.site-header');
+        if (!siteHeader.hasClass('transparent-header')) {
+            let newHeight = 0;
+            siteHeader.removeAttr('style');
+            newHeight = siteHeader.outerHeight();
+            siteHeader.height(newHeight);
+        }
+    }
+    /*---------------------
+    === Document Ready  ===
+    ----------------------*/
+    $(document).ready(function () {
+        mainMenu();
+        headerBreakpointResize();
+    });
+
+    /*--------------------
+    === Window Resize  ===
+    --------------------*/
+    $(window).on('resize', function () {
+        headerBreakpointResize();
+    });
+
+    /*--------------------
+    === Window Scroll  ===
+    ----------------------*/
+    $(window).on('scroll', function () {
+        stickyHeader();
+    });
+
+})(jQuery);
